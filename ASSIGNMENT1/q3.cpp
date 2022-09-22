9e0cae8d0175c17f98f820681e205a2fe1785ff4
Samir612153 <96933769+Samir612153@users.noreply.github.com>
2022-09-22 21:37:51 +0530

    Create q3.cpp

---
+++ ASSIGNMENT1/q3.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int integer;
+    long long long1;
+    char character;
+    float floatnumber;
+    double doublenumber;
+    
+    cin>>integer>>long1>>character>>floatnumber>>doublenumber;
+     
+    
+    cout << setprecision(20) << integer<< endl;
+    cout << setprecision(20) << long1<< endl;
+    cout << setprecision(20) << character<< endl;
+    cout << setprecision(20) << floatnumber<< endl;
+    cout << setprecision(20) << doublenumber<< endl;  
+    return 0;
+}
